a998099a50dbcfcc57d9b11c25b0acbf26b0387e
danishirfan21 <44131991+danishirfan21@users.noreply.github.com>
2026-03-03 23:44:37 +0500

    register api fix

--- server/models/User.js
+++ server/models/User.js
@@ -7,10 +7,9 @@ const UserSchema = new mongoose.Schema({
   role: { type: String, enum: ['admin', 'editor'], default: 'admin' }
 });
 
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 UserSchema.methods.comparePassword = function(candidatePassword) {
